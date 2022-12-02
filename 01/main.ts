eefdc9757dca76ac77413db6ffde5f0badfd5e84
Siyan Panayotov <contact@siyanpanayotov.com>
2022-12-02 10:43:13 +0100

    Solve day 01, part 2

--- 01/main.ts
+++ 01/main.ts
@@ -2,14 +2,14 @@ import { readLines } from "https://deno.land/std@0.167.0/io/mod.ts";
 
 const INPUT_FILENAME = "input.txt";
 
-const calories: number[] = [];
+const caloriesByElf: number[] = [];
 
 let currentElfCalories = 0;
 const inputFile = await Deno.open(INPUT_FILENAME);
 
 for await (const line of readLines(inputFile)) {
     if (line === "") {
-        calories.push(currentElfCalories);
+        caloriesByElf.push(currentElfCalories);
         currentElfCalories = 0;
     } else {
         currentElfCalories += parseInt(line);
@@ -18,8 +18,13 @@ for await (const line of readLines(inputFile)) {
 
 inputFile.close();
 
-calories.push(currentElfCalories);
+caloriesByElf.push(currentElfCalories);
 
-const maxCaloriesBySingleElf = Math.max.apply(null, calories);
+caloriesByElf.sort((a, b) => b - a);
 
-console.log(`Most calories by a single elf: ${maxCaloriesBySingleElf}`);
+console.log(`Most calories by a single Elf: ${caloriesByElf[0]}`);
+console.log(
+    `Total calories by top three Elves: ${
+        caloriesByElf.slice(0, 3).reduce((sum, a) => sum + a, 0)
+    }`,
+);
